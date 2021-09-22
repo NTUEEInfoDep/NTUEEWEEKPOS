10bdce4602f9e2820ac747334acbf7116e398922
Mason Chen <b08901058@ntu.edu.tw>
2021-09-22 22:06:47 +0800

    add coupon feature finished

--- utils/coupon.py
+++ utils/coupon.py
@@ -1,6 +1,5 @@
-import requests, json
-
-
+import requests, json, string, secrets, argparse
+from prettytable import PrettyTable
 
 class API:
     def __init__(self):
@@ -20,49 +19,66 @@ class API:
             print("Request successful!")
             print("All coupon information is listed below:")
             data = response.json()["data"]
+            
+            table = PrettyTable()
+            table.field_names = ["ID", "Name", "Code", "Discount", "Redemptions"]
+            
             for coupon in data:
                 ID = coupon["id"]
                 attributes = coupon["attributes"]
                 name = attributes["name"]
                 code = attributes["code"]
                 discount = attributes["discount"]
-                redemptions = attributes["redemptions"]
-                print(f'ID: {ID}, Name: {name:15} ,Code: {code:10}, Discount: {discount:5} $, Redemptions: {redemptions}')
-            print("")
-        
+                redemptions = attributes["redemptions"]                
+                table.add_row([ID, name, code, discount, redemptions])
+            print(table)
         elif response.status_code == 401:
             message = response.json()["message"]
-            print("Action Failed: %s\n" % message)
-        
+            print("Action Failed: %s\n" % message)        
         else:
             print("Action Failed: %s\n" % response.status_code)
 
-    def add_coupon(self,):
-        payload = {
-            "name": "Test",
-            "code": "test",
-            "type": "F",
-            "discount": 10,
-            "redemptions": 0,
-            "customer_redemptions": 0,
-            "validity": "forever",
-            "status": 1
-        }
+    def generate_code(self):
+        alphabet = string.ascii_letters + string.digits
+        code = ''.join(secrets.choice(alphabet) for i in range(6))  # for a 20-character password
+        return code
+
+    def add_coupons(self, args):
+        [filename, n] = args
+
+        with open(filename, 'r') as f:
+            coupon_model = json.load(f)
         
-        response = requests.post(self.url, headers=self.headers, params=payload)
-        
-        if response.status_code == 200:
-            print(response.json())
-        else:
-            print(response.status_code)
-            print("Action Failed: %s" % response.json()["message"])
-            print("Error: %s" % response.json())
+        for i in range(int(n)):
+            payload = coupon_model.copy()
+            payload["code"] = self.generate_code()
+            
+            response = requests.post(self.url, headers=self.headers, params=payload)
+            
+            if response.status_code == 201:
+                print(response.json())
+            else:
+                print(response.status_code)
+                print("Action Failed: %s" % response.json()["message"])
+                print("Error: %s" % response.json())
 
 def main():
-    api = API()
-    api.show_coupons()
-    api.add_coupon()
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--show", action="store_true",
+                    help="show existing coupons")
+    group.add_argument("--add", type=str, nargs=2,
+                    help="add coupon with model json file")
+    args = parser.parse_args()
 
+    api = API()
+
+    if args.show:
+        api.show_coupons()
+        print("show existing coupons")
+    elif args.add:
+        api.add_coupons(args.add)
+        print("add coupon with json file")
 
 if __name__ == "__main__":
     main()
